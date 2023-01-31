cba08cfec3eb1a8ae0bca98579cbcfc1669c713f
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-01-31 07:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+let btn=document.getElementById("addTodoBtn")
+btn.addEventListener("click",funcall)
+function funcall(){
+	let store=todoList.createElement("li")
+	store.innerText=document.getElementById("newTodoInput").value
+    todoList.appendChild(store)
+}
